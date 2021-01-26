73714c9170996460000aaf1c298e26bd63e4f4db
Amimul <amimulahsan7@gmail.com>
2021-01-26 19:31:54 +0600

    Defining first header for portfolio website.

---
+++ src/main/java/com/neocyber/neocyber/NeocyberApplication.java
@@ -0,0 +1,13 @@
+package com.neocyber.neocyber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeocyberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeocyberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neocyber/neocyber/NeocyberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neocyber.neocyber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeocyberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
