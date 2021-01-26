900c1f7fb77e90ba6182cd4adf4eece5066f7482
Amimul <amimulahsan7@gmail.com>
2021-01-27 02:34:06 +0600

    User Registration Via mail is activated. Need to change the user interface for login.

---
+++ src/main/java/com/neocyber/neocyber/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.neocyber.neocyber.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/neocyber/neocyber/appuser/AppUserService.java
@@ -0,0 +1,65 @@
+package com.neocyber.neocyber.appuser;
+
+import com.neocyber.neocyber.registration.token.ConfirmationToken;
+import com.neocyber.neocyber.registration.token.ConfirmationTokenService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class AppUserService implements UserDetailsService
+{
+    private final static String MESSAGE = "user with email %s not found";
+    private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(s)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(String.format(MESSAGE, s)));
+    }
+
+    @Transactional
+    public String signUpUser(AppUser appUser)
+    {
+        boolean userExists = appUserRepository.findByEmail(appUser.getEmail())
+                .isPresent();
+
+        if(userExists)
+        {
+            throw new IllegalStateException("Email already exists");
+        }
+
+        String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+
+        appUser.setPassword(encodedPassword);
+
+        appUserRepository.save(appUser);
+
+        String token = UUID.randomUUID().toString();
+
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                appUser
+        );
+
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+        return token;
+    }
+
+    public int enableAppUser(String email) {
+        return appUserRepository.enableAppUser(email);
+    }
+}
